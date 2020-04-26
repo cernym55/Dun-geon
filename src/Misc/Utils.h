5a09be71f0cd66e7dfd57cb6ce12ef49f268af14
Matyas Cerny <cernym55@fit.cvut.cz>
2020-04-26 18:39:22 +0200

    Added Min() and Max() templates to Utils

--- src/Misc/Utils.h
+++ src/Misc/Utils.h
@@ -17,3 +17,31 @@ bool fileExists(const std::string& filename);
  * @return int |x|
  */
 int Abs(int x);
+
+/**
+ * @brief Returns the lower of the two values
+ * 
+ * @tparam T comparable type
+ * @param first first value
+ * @param second second value
+ * @return const T& the lower of the two
+ */
+template<typename T>
+const T& Min(const T& first, const T& second)
+{
+    return first < second ? first : second;
+}
+
+/**
+ * @brief Returns the higher of the two values
+ * 
+ * @tparam T comparable type
+ * @param first first value
+ * @param second second value
+ * @return const T& the higher of the two
+ */
+template<typename T>
+const T& Max(const T& first, const T& second)
+{
+    return first < second ? second : first;
+}
